3d4accd644a97c30453a9e76d416e8882bbf4747
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-11-19 12:00:15 -0300

    Banco de Dados e Persistência em Java - JPA

--- src/test/java/br/edu/infnet/orm/model/persistencia/FornecedorDAOTest.java
+++ src/test/java/br/edu/infnet/orm/model/persistencia/FornecedorDAOTest.java
@@ -31,18 +31,24 @@ public class FornecedorDAOTest {
     @Test
     public void testIncluir() {
         IDAO fornecedorDAO = new FornecedorDAO();
+
         Fornecedor fornecedor = new Fornecedor("Maquinas Omil", "35.417.536/0001-91", "Rua Dr Getulio Vargas", 3563, "", "Bela Vista", 89140000, "Ibirama");
 
         boolean registroIncluido = fornecedorDAO.incluir(fornecedor);
+
         Assert.assertTrue(registroIncluido);
     }
 
     @Test
     public void testAlterarPrimeiroRegistro() {
         IDAO fornecedorDAO = new FornecedorDAO();
-        List<Fornecedor> listaTodos = fornecedorDAO.listarTodos();
-        Fornecedor fornecedor = new Fornecedor( listaTodos.get(0).getId(), "Teste Alteração do Fornecedor", "13.219.981/0001-04", "Teste Alteracao do Endereco", 1234, "Teste Alteracao Complemento", "Teste Alteracao Bairro", 12345678, "Teste Alteracao da Cidade");
+
+        List<Fornecedor> listaFornecedores = fornecedorDAO.listarTodos();
+
+        Fornecedor fornecedor = new Fornecedor( listaFornecedores.get(0).getId(), "Teste Alteração do Fornecedor", "13.219.981/0001-04", "Teste Alteracao do Endereco", 1234, "Teste Alteracao Complemento", "Teste Alteracao Bairro", 12345678, "Teste Alteracao da Cidade");
+
         boolean registroAlterado = fornecedorDAO.alterar(fornecedor);
+
         Assert.assertTrue(registroAlterado);
     }
 
@@ -50,9 +56,9 @@ public class FornecedorDAOTest {
     public void testExcluirUltimoRegistro() {
         IDAO fornecedorDAO = new FornecedorDAO();
 
-        List<Fornecedor> lista = fornecedorDAO.listarTodos();
+        List<Fornecedor> listaFornecedores = fornecedorDAO.listarTodos();
 
-        boolean registroExcluido = fornecedorDAO.excluir(lista.get(lista.size()-1));;
+        boolean registroExcluido = fornecedorDAO.excluir(listaFornecedores.get(listaFornecedores.size()-1));;
 
         Assert.assertTrue(registroExcluido);
     }
@@ -66,6 +72,19 @@ public class FornecedorDAOTest {
         listaFornecedores.forEach(fornecedores -> System.out.println(fornecedores));
 
         Assert.assertTrue(listaFornecedores.size() > 0);
+    }
+
+    @Test
+    public void testListarPrimeiroFornecedor() {
+        IDAO fornecedorDAO = new FornecedorDAO();
+
+        List<Fornecedor> listaFornecedores = fornecedorDAO.listarTodos();
+
+        Object fornecedor = fornecedorDAO.buscarPeloId(listaFornecedores.get(0).getId());
+
+        System.out.println(fornecedor);
+
+        Assert.assertNotNull(fornecedor);
     }
 
 }
