71691eb743ddc0af48f4aa3639731042bdfe05f5
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-11-20 11:04:18 -0300

    Banco de Dados e Persistência em Java - JPA

---
+++ src/main/java/br/edu/infnet/orm/modelo/entidade/Produto.java
@@ -0,0 +1,18 @@
+package br.edu.infnet.orm.modelo.entidade;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long codigo;
+    private String descricao;
+    private float valor;
+
+}
