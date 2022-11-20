e431dd5a782c2c3132b07d4dea92451c82529d6c
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-11-20 11:12:51 -0300

    Banco de Dados e Persistência em Java - JPA

--- src/test/java/br/edu/infnet/orm/model/persistencia/ProdutoDAOTest.java
+++ src/test/java/br/edu/infnet/orm/model/persistencia/ProdutoDAOTest.java
@@ -1,12 +1,35 @@
 package br.edu.infnet.orm.model.persistencia;
 
+import br.edu.infnet.orm.modelo.entidade.Fornecedor;
 import br.edu.infnet.orm.modelo.entidade.Produto;
+import br.edu.infnet.orm.modelo.persistencia.FornecedorDAO;
 import br.edu.infnet.orm.modelo.persistencia.IDAO;
 import br.edu.infnet.orm.modelo.persistencia.ProdutoDAO;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ProdutoDAOTest {
+
+    @Before
+    public void inicializar() {
+        IDAO produtoDAO = new ProdutoDAO();
+
+        Produto produto = new Produto( 45L, "cloridrato de tramal 50mg, ampola de 1ml", 15.6F);
+        produtoDAO.incluir(produto);
+
+        produto = new Produto( 75L, "cola para madeira 100g", 33.45F);
+        produtoDAO.incluir(produto);
+
+        produto = new Produto( 125L, "escada de fibra de vidro 7 degraus ", 96.15F);
+        produtoDAO.incluir(produto);
+
+        produto = new Produto( 137L, "escova de lavar roupa, formato anatômico, base com medidas mínimas de 11x6 cm, corpo (base) em plás", 34.5F);
+        produtoDAO.incluir(produto);
+
+        produto = new Produto( 425L, "espátula de inox para resina.", 9.99F);
+        produtoDAO.incluir(produto);
+    }
 
     @Test
     public void incluir() {
