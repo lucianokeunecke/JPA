a1dd177cae39d5ec0d6fc31cf4e4d48bf0bbc35e
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-11-20 11:19:58 -0300

    Banco de Dados e Persistência em Java - JPA

--- src/test/java/br/edu/infnet/orm/model/persistencia/ProdutoDAOTest.java
+++ src/test/java/br/edu/infnet/orm/model/persistencia/ProdutoDAOTest.java
@@ -15,22 +15,26 @@ public class ProdutoDAOTest {
 
     @Before
     public void inicializar() {
-        /*IDAO produtoDAO = new ProdutoDAO();
+        IDAO produtoDAO = new ProdutoDAO();
 
-        Produto produto = new Produto( 45L, "cloridrato de tramal 50mg, ampola de 1ml", 15.6F);
-        produtoDAO.incluir(produto);
+        List<Produto> listaProdutos = produtoDAO.listarTodos();
 
-        produto = new Produto( 75L, "cola para madeira 100g", 33.45F);
-        produtoDAO.incluir(produto);
+        if (listaProdutos.size() == -1) {
+            Produto produto = new Produto( 45L, "cloridrato de tramal 50mg, ampola de 1ml", 15.6F);
+            produtoDAO.incluir(produto);
 
-        produto = new Produto( 125L, "escada de fibra de vidro 7 degraus ", 96.15F);
-        produtoDAO.incluir(produto);
+            produto = new Produto( 75L, "cola para madeira 100g", 33.45F);
+            produtoDAO.incluir(produto);
 
-        produto = new Produto( 137L, "escova de lavar roupa, formato anatômico, base com medidas mínimas de 11x6 cm, corpo (base) em plás", 34.5F);
-        produtoDAO.incluir(produto);
+            produto = new Produto( 125L, "escada de fibra de vidro 7 degraus ", 96.15F);
+            produtoDAO.incluir(produto);
 
-        produto = new Produto( 425L, "espátula de inox para resina.", 9.99F);
-        produtoDAO.incluir(produto);*/
+            produto = new Produto( 137L, "escova de lavar roupa, formato anatômico, base com medidas mínimas de 11x6 cm, corpo (base) em plás", 34.5F);
+            produtoDAO.incluir(produto);
+
+            produto = new Produto( 425L, "espátula de inox para resina.", 9.99F);
+            produtoDAO.incluir(produto);
+        }
     }
 
     @Test
